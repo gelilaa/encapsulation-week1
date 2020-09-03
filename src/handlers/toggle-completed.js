9927969645b129c31df6c5ec1c8abc6a420f3062
gelila <gelila.ali@outlook.com>
2020-09-03 22:13:31 +0200

    added remove closes #5

--- src/handlers/toggle-completed.js
+++ src/handlers/toggle-completed.js
@@ -17,6 +17,36 @@ debugger;
     })
   document.getElementById('add').value = '';
   },
+   /*changeTodo: function () {
+       var position = document.getElementById('position');
+       var text = document.getElementById('text');
+       app.changeTodo(position.valueAsNumber, text.value);
+       position.value = '';
+       text.value = '';
+       view.displayTodos();
+     },*/
+     deleteTodo: function (position) {
+       debugger
+       app.deleteTodo(position);
+       view.displayTodos();
+     },
+     toggleCompleted: function () {
+       debugger;
+       var address = document.querySelector('ul');
+       var toggleCompletedPo = address.childNodes;
+       toggleCompletedPo.forEach(function (child) {
+         if (child.childNodes[0].checked === true) {
+           var input = child.id;
+           app.toggleCompleted(Number(input));
+           child.className = 'checkbox';
+
+         };
+       });
+       logger.push({
+         action: 'toggle completed',
+         state: app.state
+       });
+     },
     
   }
   
