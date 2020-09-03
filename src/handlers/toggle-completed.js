1fa822d45b970311274ee3d575316120cd6c8b93
gelila <gelila.ali@outlook.com>
2020-09-03 22:04:40 +0200

    touch-ups

--- src/handlers/toggle-completed.js
+++ src/handlers/toggle-completed.js
@@ -4,6 +4,10 @@ debugger;
  e = e || window.event;
  e.preventDefault();
       var add = document.getElementById('add').value;
+      if (add === '') {
+        alert('Please write a Todo!');
+        return;
+      }
       app.addTodo(add);
       view.displayTodos();
     
