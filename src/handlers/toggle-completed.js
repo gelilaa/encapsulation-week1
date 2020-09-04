be67b6c81a5f782701920ae79f35ae3e4b1a6e0c
gelila <gelila.ali@outlook.com>
2020-09-04 11:04:11 +0200

    added show completed & listeners closes #6 & #7

--- src/handlers/toggle-completed.js
+++ src/handlers/toggle-completed.js
@@ -1,23 +1,23 @@
 var handlers = {
-    addTodos: function (e) {
-debugger;
- e = e || window.event;
- e.preventDefault();
-      var add = document.getElementById('add').value;
-      if (add === '') {
-        alert('Please write a Todo!');
-        return;
-      }
-      app.addTodo(add);
-      view.displayTodos();
-    
+  addTodos: function (e) {
+    debugger;
+    e = e || window.event;
+    e.preventDefault();
+    var add = document.getElementById("add").value;
+    if (add === "") {
+      alert("Please write a Todo!");
+      return;
+    }
+    app.addTodo(add);
+    view.displayTodos();
+
     logger.push({
-      action: 'Add Todo',
-      state: app.state
-    })
-  document.getElementById('add').value = '';
+      action: "Add Todo",
+      state: app.state,
+    });
+    document.getElementById("add").value = "";
   },
-   /*changeTodo: function () {
+  /*changeTodo: function () {
        var position = document.getElementById('position');
        var text = document.getElementById('text');
        app.changeTodo(position.valueAsNumber, text.value);
@@ -25,28 +25,30 @@ debugger;
        text.value = '';
        view.displayTodos();
      },*/
-     deleteTodo: function (position) {
-       debugger
-       app.deleteTodo(position);
-       view.displayTodos();
-     },
-     toggleCompleted: function () {
-       debugger;
-       var address = document.querySelector('ul');
-       var toggleCompletedPo = address.childNodes;
-       toggleCompletedPo.forEach(function (child) {
-         if (child.childNodes[0].checked === true) {
-           var input = child.id;
-           app.toggleCompleted(Number(input));
-           child.className = 'checkbox';
-
-         };
-       });
-       logger.push({
-         action: 'toggle completed',
-         state: app.state
-       });
-     },
-    
-  }
+  deleteTodo: function (position) {
+    debugger;
+    app.deleteTodo(position);
+    view.displayTodos();
+  },
+  toggleCompleted: function () {
+    debugger;
+    var address = document.querySelector("ul");
+    var toggleCompletedPo = address.childNodes;
+    toggleCompletedPo.forEach(function (child) {
+      if (child.childNodes[0].checked === true) {
+        var input = child.id;
+        app.toggleCompleted(Number(input));
+        child.className = "checkbox";
+      }
+    });
+    logger.push({
+      action: "toggle completed",
+      state: app.state,
+    });
+  },
+  toggleAll: function () {
+    app.toggleAll();
+    view.displayTodos();
+  },
+};
   
